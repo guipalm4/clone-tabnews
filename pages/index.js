6ccd6a7c0bfbde24c36b0c9c2fb26c6b8100adc7
Guilherme Palma <guilherme.palma.ext@dasa.com.br>
2024-07-05 17:55:22 -0300

    add endpoint `api/v1/status`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 function Home() {
   return (
     <div>
-      <h1>Te amo Victória</h1>
+      <h1>O Thobias não sabe ligar a CDJ</h1>
+      <img src="/assets/thobias.gif" alt="Descrição do GIF" />
+      <div>
+        <h1>O curupira sabe!</h1>
+        <img src="/assets/edilson.gif" alt="Descrição do GIF" />
+      </div>
     </div>
   );
 }
