8e2948f3923138b2a38bb91d1b7b36150a93c66f
Guilherme Palma <guilherme.palma.ext@dasa.com.br>
2024-07-08 15:07:00 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status shpould return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody).toBeDefined();
+  expect(responseBody).toHaveProperty("updated_at");
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const databaseInfo = responseBody.dependencies.database;
+
+  expect(databaseInfo.version).toEqual("16.3");
+  expect(databaseInfo.max_connections).toEqual(100);
+  expect(databaseInfo.opened_connections).toEqual(1);
 });
