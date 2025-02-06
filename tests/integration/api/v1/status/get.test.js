47e82ee37edd9a2663c458a1e9a07402c7d6a57e
guipalm4 <gomes.lmc@gmail.com>
2025-02-06 10:42:05 -0300

    test: rename all descriptions to new use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,22 +4,26 @@ beforeAll(async () => {
   await orchestrator.waitForAllService();
 });
 
-test("GET to /api/v1/status shpould return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(responseBody).toBeDefined();
-  expect(responseBody).toHaveProperty("updated_at");
+      expect(responseBody).toBeDefined();
+      expect(responseBody).toHaveProperty("updated_at");
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  const databaseInfo = responseBody.dependencies.database;
+      const databaseInfo = responseBody.dependencies.database;
 
-  expect(databaseInfo.version).toEqual("16.3");
-  expect(databaseInfo.max_connections).toEqual(100);
-  expect(databaseInfo.opened_connections).toEqual(1);
+      expect(databaseInfo.version).toEqual("16.3");
+      expect(databaseInfo.max_connections).toEqual(100);
+      expect(databaseInfo.opened_connections).toEqual(1);
+    });
+  });
 });
