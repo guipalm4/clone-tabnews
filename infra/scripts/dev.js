14cdd20519315a08614bdfe7730dd81e750fbfe1
guipalm4 <gomes.lmc@gmail.com>
2025-02-06 11:00:06 -0300

    style: apply prettier fix in `dev.js`

--- infra/scripts/dev.js
+++ infra/scripts/dev.js
@@ -1,13 +1,17 @@
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
 
 function execCommand(command, args = []) {
   // eslint-disable-next-line no-undef
   return new Promise((resolve, reject) => {
-    const child = spawn(command, args, { stdio: 'inherit', shell: true });
-    child.on('error', (error) => reject(error));
-    child.on('exit', (code) => {
+    const child = spawn(command, args, { stdio: "inherit", shell: true });
+    child.on("error", (error) => reject(error));
+    child.on("exit", (code) => {
       if (code !== 0) {
-        reject(new Error(`"${command} ${args.join(' ')}" finalizou com código ${code}`));
+        reject(
+          new Error(
+            `"${command} ${args.join(" ")}" finalizou com código ${code}`,
+          ),
+        );
       } else {
         resolve();
       }
@@ -17,36 +21,36 @@ function execCommand(command, args = []) {
 
 async function run() {
   try {
-    await execCommand('npm', ['run', 'services:up']);
-    await execCommand('npm', ['run', 'services:wait:database']);
-    await execCommand('npm', ['run', 'migrations:up']);
+    await execCommand("npm", ["run", "services:up"]);
+    await execCommand("npm", ["run", "services:wait:database"]);
+    await execCommand("npm", ["run", "migrations:up"]);
   } catch (err) {
-    console.error('Erro ao executar comandos iniciais:', err);
+    console.error("Erro ao executar comandos iniciais:", err);
     process.exit(1);
   }
 
-  const nextDev = spawn('next', ['dev'], { stdio: 'inherit', shell: true });
+  const nextDev = spawn("next", ["dev"], { stdio: "inherit", shell: true });
 
   const cleanup = async () => {
     if (cleanup.called) return;
     cleanup.called = true;
 
-    console.log('\n ❗️Interrupção detectada! ✋🏼Parando os serviços...');
+    console.log("\n ❗️Interrupção detectada! ✋🏼Parando os serviços...");
     try {
       if (!nextDev.killed) {
-        nextDev.kill('SIGINT');
+        nextDev.kill("SIGINT");
       }
-      await execCommand('npm', ['run', 'services:stop']);
+      await execCommand("npm", ["run", "services:stop"]);
     } catch (err) {
-      console.error('Erro ao parar os serviços:', err);
+      console.error("Erro ao parar os serviços:", err);
     } finally {
       process.exit();
     }
   };
 
-  process.on('SIGINT', cleanup);
-  process.on('SIGTERM', cleanup);
-  nextDev.on('exit', cleanup);
+  process.on("SIGINT", cleanup);
+  process.on("SIGTERM", cleanup);
+  nextDev.on("exit", cleanup);
 }
 
 run();
