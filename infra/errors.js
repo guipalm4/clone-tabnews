2ca95c111ee83d42ede0a4aa526220f8983c5375
guipalm4 <gomes.lmc@gmail.com>
2025-02-17 12:16:01 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,21 +1,57 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("An unexpected error occurred.", {
       cause,
     });
     this.name = "InternalServerError";
-    this.cause = "Please contact support.";
-    this.statusCode = 500;
+    this.action = "Please contact support.";
+    this.statusCode = statusCode || 500;
   }
 
   toJSON() {
     return {
-      error: {
-        name: this.name,
-        message: this.message,
-        action: this.cause,
-        status_code: this.statusCode,
-      },
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service temporarily unavailable.", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Verify if the service is running.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method Not Allowed for this route.");
+    this.name = "MethodNotAllowedError";
+    this.action = "Verify if the HTTP method is valid for the route.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
     };
   }
 }
